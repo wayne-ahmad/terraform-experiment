b1c02095de8593af42685164d68c8adbaca669ba
wayne-ahmad <wayne.ahmad@jisc.ac.uk>
2022-12-02 17:15:09 +0000

    added hashing for zip upload

--- terraform/main.tf
+++ terraform/main.tf
@@ -61,7 +61,7 @@ resource "aws_lambda_function" "lambda" {
 
   function_name = var.lambda_config.function_name
   # filename         = data.archive_file.lambda.output_path
-  source_code_hash = data.archive_file.lambda.output_base64sha256
+  source_code_hash = data.local_file.deploy-zip.output_base64sha256
   s3_bucket        = aws_s3_bucket.deployment-bucket.bucket
   s3_key           = aws_s3_object.file_upload.key
   role             = aws_iam_role.lambda_role.arn
@@ -91,13 +91,13 @@ resource "aws_cloudwatch_log_group" "lambda_function_log" {
 
 # deployment_zip_file.
 
-data "archive_file" "lambda" {
-  type        = "zip"
-  source_dir  = "../"
-  output_path = "../temp/lambda.zip"
-  excludes    = ["src/tests.py", ".git", ".gitignore", "temp", "ignoreme", "terraform"]
+# data "archive_file" "lambda" {
+#   type        = "zip"
+#   source_dir  = "../"
+#   output_path = "../temp/lambda.zip"
+#   excludes    = ["src/tests.py", ".git", ".gitignore", "temp", "ignoreme", "terraform"]
 
-}
+# }
 
 # find local file deploy.zip
 data "local_file" "deploy-zip" {
